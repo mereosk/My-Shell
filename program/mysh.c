5ee064d38846107111109d3d881ea3d1e9268273
mereosk <mereosk@yahoo.gr>
2023-04-05 00:48:44 +0300

    something

--- program/mysh.c
+++ program/mysh.c
@@ -8,7 +8,7 @@
 int main(int argc, char** argv) {
 
     List mylist = list_create(NULL);
-    char yo[30];
+    char yo[40];
     while(1) {
         printf("\nin-mysh-now:>");
         scanf("%s", yo);
